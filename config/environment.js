cb8bac336975129103188f19e444d9915b67b2ed
Joe <forsmann@frontier.com>
2015-08-05 06:13:53 -0700

    Fix routing in prod

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'art-day',
     environment: environment,
     baseURL: '/',
-    locationType: 'hash',
+    locationType: 'history',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
