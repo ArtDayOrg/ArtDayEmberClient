95abee6572ee8775643721f644bdf2da9b35e156
Joe <forsmann@frontier.com>
2015-08-08 05:10:05 -0700

    Moving sessions under admin route.  Adding imageURL field to sessions.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,18 +1,9 @@
-
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
 	var app = new EmberApp(defaults);
 	app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 	app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-
-    minifyCSS: {
-        enabled: false
-    }
-
-    minifyJS: {
-        enabled: false
-    }
 
 	return app.toTree();
 };
