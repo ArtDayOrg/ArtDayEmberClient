933ea731009935e8928dd6fd37b3246f168bb59c
Joe <forsmann@frontier.com>
2015-08-14 20:09:00 -0700

    First shot at adding facebook auth.

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -1,8 +1,30 @@
 import Ember from 'ember';
+import FB from 'ember-cli-facebook-js-sdk/fb';
 
 export default Ember.Controller.extend({
     // will add auth later
     isAdmin: true,
+    isLoggedIn: function() {
+        console.log('inside isLoggedIn method.');
+        FB.getLoginStatus(function(response) {
+          if (response.status === 'connected') {
+            // the user is logged in and has authenticated your
+            // app, and response.authResponse supplies
+            // the user's ID, a valid access token, a signed
+            // request, and the time the access token 
+            // and signed request each expire
+            var uid = response.authResponse.userID;
+            var accessToken = response.authResponse.accessToken;
+          } else if (response.status === 'not_authorized') {
+            // the user is logged in to Facebook, 
+            // but has not authenticated your app
+          } else {
+            // the user isn't logged in to Facebook.
+          }
+        }, function(error) {
+            console.log(error);
+        });
+    }.property(),
     actions: {
         
     }
