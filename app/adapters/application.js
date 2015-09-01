795969d50b7fe37e41499e0ae548fd4859a0af82
Joe <forsmann@frontier.com>
2015-08-31 20:34:45 -0700

    re-adding ENV.APP.host

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,8 +2,7 @@ import DS from 'ember-data';
 import ENV from '../config/environment';
 
 export default DS.RESTAdapter.extend({
-  // host: 'http://artday.azurewebsites.net',    
-  host: 'http://artday.azurewebsites.net',
+  host: ENV.APP.host,
   namespace: 'api',
   shouldReloadAll: function() { return false; }
 });
