1db3ec8a2b521a2452dc1644a7b5e650ed380984
BrianSKing <Dev@Brians-MacBook-Pro.local>
2015-09-19 16:50:24 -0700

    fixed bugs in session-tile-draggable
    fixed bug where image could disappear and you could drag the
    transparent pixel (by setting transparency on drag, not on dragStart).
    
    fixed bug where session name was disappearing from tile ghost when
    dragged.
    
    fixed bug where session-tile-draggable did not correctly bubble
    changeDescription action

--- app/components/session-tile-draggable.js
+++ app/components/session-tile-draggable.js
@@ -1,16 +1,33 @@
 import Ember from 'ember';
+import ImagePath from 'art-day/mixins/image-path';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(ImagePath, {
 
   isTransparent: false,
 
-  dragStart: function() {
-    console.log('test');
+  transparentUrl: "assets/images/transparent.png",
+
+  imageUrl: function() {
+    console.log('isTransparent', this.get('imagePath'));
+    return this.get('isTransparent') ? this.get('transparentUrl') : this.get('imagePath');
+  }.property('imagePath', 'isTransparent'),
+
+  drag: function() {
     this.set('isTransparent', true);
   },
 
   dragEnd: function() {
     this.set('isTransparent', false);
+  },
+
+  drop: function() {
+    this.set('isTransparent', false);
+  },
+
+  actions: {
+    changeDescription: function(description, sessionName) {
+      this.sendAction('changeDescription', description, sessionName);
+    }
   }
 
 });
--- app/components/session-tile.js
+++ app/components/session-tile.js
@@ -4,12 +4,6 @@ import ImagePath from 'art-day/mixins/image-path';
 export default Ember.Component.extend(ImagePath, {
   classNames: ['session-tile'],
 
-  transparentUrl: "assets/images/transparent.png",
-
-  imageUrl: function() {
-    return this.get('isTransparent') ? this.get('transparentUrl') : this.get('imagePath');
-  }.property('imagePath', 'isTransparent'),
-
   click: function() {
     this.sendAction('changeDescription', this.get('item.description'), this.get('item.sessionName'));
   }
