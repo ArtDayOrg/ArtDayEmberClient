abdbd3271ed4f44fae10f06592c445cbea381752
BrianSKing <Dev@Brians-MacBook-Pro.local>
2016-01-12 19:21:32 -0800

    Update schedule to reflect new data

--- app/mixins/student-schedule.js
+++ app/mixins/student-schedule.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
 
-  schedule: function () {
+  schedule: function() {
     var sessionsArray = ['Period 1', 'Period 2', 'Period 3'];
     var locationsArray = ['', '', ''];
     var item = this.get('item');
     var self = this;
     if (item) {
-      item.get('enrollments').forEach(function (enrollment) {
+      item.get('enrollments').forEach(function(enrollment) {
         if (enrollment.get('session.sessionName') && enrollment.get('period') && enrollment.get('session.location')) {
           var sessionString = enrollment.get('session.sessionName');
           var locationString = enrollment.get('session.location');
@@ -21,11 +21,12 @@ export default Ember.Mixin.create({
     }
 
     var schedule = [
-    ['8:00AM', '9:30AM', sessionsArray[0], locationsArray[0]],
-    ['9:40AM', '11:10AM', sessionsArray[1], locationsArray[1]],
-    ['11:20AM', '12:15PM', 'Lunch', 'Cafeteria'],
-    ['12:25PM', '1:15PM', 'Assembly', 'Gym'],
-    ['1:30PM', '3:00PM', sessionsArray[2], locationsArray[2]]
+      ['7:30 AM', '7:35 AM', 'Homeroom', 'Homeroom'],
+      ['7:40 AM', '9:05 AM', sessionsArray[0], locationsArray[0]],
+      ['9:10 AM', '10:35 AM', sessionsArray[1], locationsArray[1]],
+      ['10:40 AM', '11:40 AM', 'Assembly', 'Lyceum'],
+      ['11:50 AM', '12:20 PM', 'Lunch', 'Lyceum'],
+      ['12:30 PM', '2:05 PM', sessionsArray[2], locationsArray[2]]
     ];
 
     return schedule;
